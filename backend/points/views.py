a571a09c01f76d29ec37566b8727f1e54fc45f5b
Антон Краснокутский <anton.krasnokutsky@gmail.com>
2023-10-08 10:55:48 +0300

    Поле выбора точек

--- backend/points/views.py
+++ backend/points/views.py
@@ -1,27 +1,6 @@
-# from django.shortcuts import render
-# from django.views import View
+from django.views.generic import TemplateView
 
 from .models import Points
-
-
-# class Service(View):
-#     template = 'points/service.html'
-
-#     def get(self, request, *args, **kwargs):
-#         points = Points.objects.all()
-#         context = {
-#             'data': {
-#                 'value': obj.name,
-#             }
-#             for obj in Points.objects.all()
-#             # 'points': points,
-#         }
-#         return render(
-#             request,
-#             self.template,
-#             context
-#         )
-from django.views.generic import TemplateView
 
 
 class SomeTemplateView(TemplateView):
