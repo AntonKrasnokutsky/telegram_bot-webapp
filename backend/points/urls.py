ee36a3d48d5924ccc7542f1e3a048a8c14a84866
Антон Краснокутский <anton.krasnokutsky@gmail.com>
2023-10-08 10:53:47 +0300

    Поле выбора точек

--- backend/points/urls.py
+++ backend/points/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import Service
+from .views import SomeTemplateView
 
 app_name = 'points'
 
 urlpatterns = [
-    path('service/', Service.as_view()),
+    path('service/', SomeTemplateView.as_view()),
 ]
--- backend/points/views.py
+++ backend/points/views.py
@@ -1,19 +1,41 @@
-from django.shortcuts import render
-from django.views import View
+# from django.shortcuts import render
+# from django.views import View
 
 from .models import Points
 
 
-class Service(View):
-    template = 'points/service.html'
+# class Service(View):
+#     template = 'points/service.html'
 
-    def get(self, request, *args, **kwargs):
-        points = Points.objects.all()
-        context = {
-            'points': points,
-        }
-        return render(
-            request,
-            self.template,
-            context
-        )
+#     def get(self, request, *args, **kwargs):
+#         points = Points.objects.all()
+#         context = {
+#             'data': {
+#                 'value': obj.name,
+#             }
+#             for obj in Points.objects.all()
+#             # 'points': points,
+#         }
+#         return render(
+#             request,
+#             self.template,
+#             context
+#         )
+from django.views.generic import TemplateView
+
+
+class SomeTemplateView(TemplateView):
+    template_name = 'points/service.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['data'] = [
+            {
+                'id': obj.id,
+                'value': obj.name,
+            }
+            for obj in Points.objects.all()
+        ]
+
+        return context
